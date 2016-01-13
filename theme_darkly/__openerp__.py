fd65cde8a5a7e7276577bf49c354ec2a29204cb0
OfficeEverywhere <support@office-everywhere.com>
2016-01-13 12:07:51 -0600

    beta version 0.1

---
+++ theme_darkly/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'author': "Office Everywhere",
+    'website': "https://www.office-everywhere.com",
+    'name': 'Bootswatch Darkly',
+    'summary': 'Bootswatch Darkly',
+    'description': 'Bootswatch Darkly',
+    'category': 'Theme',
+    'version': '0.1',
+    'depends': ['website'],
+    'data': ['views/bootswatch-darkly.xml'],
+    'images': ['theme_bootswatch_darkly/static/description/bootswatch.png'],
+    'application': False,
+}
