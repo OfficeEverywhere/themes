23ba6285a6609ec2bab150c4d3947148befa05b1
OfficeEverywhere <support@office-everywhere.com>
2016-01-14 09:53:47 -0600

    beta version 0.3.2

--- theme_bootswatch_native/__openerp__.py
+++ theme_bootswatch_native/__openerp__.py
@@ -5,7 +5,7 @@
     'summary': 'Native Bootswatch Themes',
     'description': 'Native Bootswatch Themes',
     'category': 'Theme',
-    'version': '0.3.1',
+    'version': '0.3.2',
     'images': ['images/bootswatch-native.png'],
     'depends': ['website'],
     'data': ['views/bootswatch-native.xml'],
